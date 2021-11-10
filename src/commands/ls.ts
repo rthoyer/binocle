cca4cffa95fe114c1e3c22e13d99ea7d994e5f3b
Romain THOYER <romain.thoyer@gmail.com>
2021-11-10 22:33:13 +0100

    Add image export feature

--- src/commands/ls.ts
+++ src/commands/ls.ts
@@ -1,6 +1,9 @@
 import {Command, flags} from '@oclif/command'
 import LookerClient, { ILookerFolder } from '../client/client'
 import ora from 'ora'
+import { createCanvas, loadImage, NodeCanvasRenderingContext2D } from 'canvas'
+import fs from 'fs'
+import open from 'open'
 
 export default class Listing extends Command {
   static description = 'Lists the content of the selected folder and its subfolders'
@@ -13,6 +16,7 @@ export default class Listing extends Command {
 `,
   ]
   public static page_limit = 50
+  public static font_size = 50
 
   static flags = {
     base_url: flags.string({
@@ -69,7 +73,7 @@ export default class Listing extends Command {
     var main_folder: ILookerFolder
     try {
       main_folder = await this.client.getFolder(args.folder_id)
-      spinner_main_folder.succeed(`Folder ${Listing.contentLinkFromId(args.folder_id,'folder')} (${main_folder.name}) found`)
+      spinner_main_folder.succeed(`Folder ${Listing.contentLinkFromId(flags.base_url, args.folder_id,'folder')} (${main_folder.name}) found`)
       var folder_org: IFolderOrganisation = {
         children: [],
         dashboards: main_folder.dashboards.length,
@@ -84,7 +88,26 @@ export default class Listing extends Command {
         spinner_subfolders.succeed(`Subfolders found`)
         if(flags.image){
           const spinner_image_export  = ora(`Exporting the image in the current folder ${process.cwd()}`).start()
-          spinner_image_export.succeed(`Image exported as ${process.cwd()}/binocle_ls_${args.folder_id}_${Date.now()}.png`)
+          try {
+            const canvas = createCanvas(200,200)
+            const canvas_context = canvas.getContext("2d")
+            canvas_context.font = `${Listing.font_size.toString()}px Arial`
+            canvas_context.textAlign = 'left'
+            const { width, lines } = this.getCanvasSize(folder_org, parseInt(flags.depth), 0, 0, canvas_context)
+            canvas_context.canvas.width = width + 2 * Listing.font_size
+            canvas_context.canvas.height = Listing.font_size * (lines + 2) 
+            canvas_context.font = `${Listing.font_size.toString()}px Arial`
+            this.writeListingInCanvas(folder_org, parseInt(flags.depth), 0, canvas_context, flags.base_url)
+            const buffer = canvas.toBuffer()
+            const filename = `${process.cwd()}/binocle_ls_${args.folder_id}_${Date.now()}.png`
+            fs.writeFileSync(filename, buffer)
+            spinner_image_export.succeed(`Image exported as ${filename}`)
+            await open(filename)
+          }
+          catch(e) {
+            spinner_image_export.fail()
+            console.error(e)
+          }
         }
         this.printListing(folder_org, parseInt(flags.depth),flags.base_url)
       }
@@ -122,15 +145,40 @@ export default class Listing extends Command {
     }
   }
 
-  public printListing(org: IFolderOrganisation, max_depth: number, link_url: string){
-    this.log(`${"|   ".repeat(org.depth)}📁 ${org.name} ${Listing.contentLinkFromId(org.id,'folder')} ${org.depth ===  max_depth ? '' : ` (D:${org.dashboards} - L:${org.looks})`}`)
+  public printListing(org: IFolderOrganisation, max_depth: number, base_url: string){
+    this.log(`${"|   ".repeat(org.depth)}📁 ${org.name} ${Listing.contentLinkFromId(base_url,org.id,'folder')} ${org.depth ===  max_depth ? '' : ` (D:${org.dashboards} - L:${org.looks})`}`)
     for (const child of org.children){
-      this.printListing(child, max_depth,link_url)
+      this.printListing(child, max_depth, base_url)
     }
   }
 
-  public static contentLinkFromId(id: string, type: looker_content){
-    return `\x1B]8;;${this.flags.base_url}/${type}s/${id}\x1B\\#${id}\x1B]8;;\x1B\\`
+  public getCanvasSize(org: IFolderOrganisation, max_depth: number, width: number, lines: number , canvas_context: NodeCanvasRenderingContext2D){
+    const line_width = canvas_context.measureText(`${"|   ".repeat(org.depth)}📁 ${org.name} #${org.id} ${org.depth ===  max_depth ? '' : ` (D:${org.dashboards} - L:${org.looks})`}`).width
+    let max_width = line_width>width ? line_width : width
+    let total_lines = lines + 1
+    for (const child of org.children){
+      const {width: new_width, lines: new_lines} = this.getCanvasSize(child, max_depth, max_width, total_lines, canvas_context)
+      max_width = new_width
+      total_lines = new_lines
+    }
+    return { width : max_width, lines : total_lines}
+  }
+
+  public writeListingInCanvas(org: IFolderOrganisation, max_depth: number, line: number, canvas_context: NodeCanvasRenderingContext2D, base_url: string){
+    let new_line = line + 1
+    canvas_context.fillText(
+      `${"|   ".repeat(org.depth)}📁 ${org.name} #${org.id} ${org.depth ===  max_depth ? '' : ` (D:${org.dashboards} - L:${org.looks})`}`,
+      Listing.font_size,
+      Listing.font_size * (new_line + 1),
+    )
+    for (const child of org.children){
+      new_line = this.writeListingInCanvas(child, max_depth, new_line, canvas_context, base_url)
+    }
+    return new_line
+  }
+
+  public static contentLinkFromId(base_url: string,id: string, type: looker_content){
+    return `\x1B]8;;${base_url}/${type}s/${id}\x1B\\#${id}\x1B]8;;\x1B\\`
   }
 
   private client!: LookerClient
